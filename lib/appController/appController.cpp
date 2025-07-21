f5f8025acda6770571bee97ee2f25531562b3681
MOK <tommok@sycratech.com>
2025-07-21 14:13:34 +0800

    feat: finally simply  works
    - Slider works
    - Clear screen works
    - move the tool bar in application side

--- lib/appController/appController.cpp
+++ lib/appController/appController.cpp
@@ -24,104 +24,105 @@
  *  STATIC PROTOTYPES
  **********************/
 
-#define CANVAS_WIDTH 200
-#define CANVAS_HEIGHT 300
 
 static lv_obj_t *_sketchpad;
 static uint8_t _drawline_flag = 0;
-/**
- * Draw a line to the canvas
- */
-// void lv_example_canvas_7(void)
-// {
-//     /*Create a buffer for the canvas*/
-//     // LV_DRAW_BUF_DEFINE(draw_buf, CANVAS_WIDTH, CANVAS_HEIGHT, LV_COLOR_FORMAT_ARGB8888);
-//     lv_draw_buf_t *draw_buf = lv_draw_buf_create(240, 320, LV_COLOR_FORMAT_ARGB8888, LV_STRIDE_AUTO);
-//     /*Create a canvas and initialize its palette*/
-//     lv_obj_t *canvas = lv_canvas_create(lv_screen_active());
-//     lv_canvas_set_draw_buf(canvas, draw_buf);
-//     lv_canvas_fill_bg(canvas, lv_color_hex3(0xccc), LV_OPA_COVER);
-//     lv_obj_center(canvas);
-
-//     lv_layer_t layer;
-//     lv_canvas_init_layer(canvas, &layer);
-
-//     lv_draw_line_dsc_t dsc;
-//     lv_draw_line_dsc_init(&dsc);
-//     dsc.color = lv_palette_main(LV_PALETTE_RED);
-//     dsc.width = 4;
-//     dsc.round_end = 1;
-//     dsc.round_start = 1;
-//     dsc.p1.x = 15;
-//     dsc.p1.y = 15;
-//     dsc.p2.x = 35;
-//     dsc.p2.y = 10;
-//     lv_draw_line(&layer, &dsc);
-
-//     lv_canvas_finish_layer(canvas, &layer);
-// }
-
+static void sketchpad_toolbar_event_cb(lv_event_t *e);
+static void toolbar_set_event_cb(lv_event_t *e);
 /**********************
  * STATIC FUNCTIONS
  **********************/
 
-static void event_handler(lv_event_t *e)
-{
-    // lv_obj_t *obj = (lv_obj_t *)lv_event_get_target(e);
 
-    _drawline_flag = 1;
-    LV_LOG_USER("LV_EVENT_VALUE_CHANGED");
-}
 /**********************
  *  LIB API
  **********************/
 void app_controller_init(void)
 {
-    // LV_DRAW_BUF_DEFINE(draw_buf, 240, 320, LV_COLOR_FORMAT_ARGB8888);
 
-    lv_draw_buf_t *draw_buf = lv_draw_buf_create(200, 200, LV_COLOR_FORMAT_ARGB8888, LV_STRIDE_AUTO);
+    lv_draw_buf_t *draw_buf = lv_draw_buf_create(240, 300, LV_COLOR_FORMAT_ARGB8888, LV_STRIDE_AUTO);
     _sketchpad = lv_100ask_sketchpad_create(lv_screen_active());
-    // lv_obj_set_scrollbar_mode(sketchpad, LV_SCROLLBAR_MODE_OFF); // See no different in PC, but may have some difference in the TFT display
-    // lv_obj_set_scroll_snap_x(sketchpad, LV_SCROLL_SNAP_NONE);
-    // lv_obj_set_scroll_snap_y(sketchpad, LV_SCROLL_SNAP_NONE);
-    // lv_obj_clear_flag(sketchpad, LV_OBJ_FLAG_SCROLL_ELASTIC); // Disable elastic scrolling>>not work
-    // lv_obj_clear_flag(sketchpad, LV_OBJ_FLAG_SCROLL_CHAIN); // THis work
+    lv_obj_set_scrollbar_mode(_sketchpad, LV_SCROLLBAR_MODE_OFF); // See no different in PC, but may have some difference in the TFT display
+    lv_obj_set_scroll_snap_x(_sketchpad, LV_SCROLL_SNAP_NONE);
+    lv_obj_set_scroll_snap_y(_sketchpad, LV_SCROLL_SNAP_NONE);
+    lv_obj_clear_flag(_sketchpad, LV_OBJ_FLAG_SCROLL_ELASTIC); // Disable elastic scrolling>>not work
+    lv_obj_clear_flag(_sketchpad, LV_OBJ_FLAG_SCROLL_CHAIN);   // THis work
 
     lv_canvas_set_draw_buf(_sketchpad, draw_buf);
-    void* ptr3 = draw_buf->data;
+    // void* ptr3 = draw_buf->data;
 
     // Serial.printf("Init: draw buffer address = %08x\r\n", ptr3);
     lv_canvas_fill_bg(_sketchpad, lv_color_hex3(0xccc), LV_OPA_COVER);
-    lv_obj_center(_sketchpad);
-    // lv_obj_add_event_cb(_sketchpad, event_handler, LV_EVENT_VALUE_CHANGED, NULL);
-    // lv_example_canvas_7();
+    lv_obj_align(_sketchpad, LV_ALIGN_BOTTOM_MID, 0, 0);
+
+
+    static lv_coord_t sketchpad_toolbar_cw = LV_100ASK_SKETCHPAD_TOOLBAR_OPT_DELETE;
+    lv_obj_t *color = lv_label_create(lv_screen_active());
+    lv_label_set_text(color, LV_SYMBOL_TRASH);
+    lv_obj_add_flag(color, LV_OBJ_FLAG_CLICKABLE);
+    lv_obj_align(color, LV_ALIGN_TOP_MID, -10, 0);
+    lv_obj_add_event_cb(color, sketchpad_toolbar_event_cb, LV_EVENT_ALL, &sketchpad_toolbar_cw);
+
+    static lv_coord_t sketchpad_toolbar_width = LV_100ASK_SKETCHPAD_TOOLBAR_OPT_WIDTH;
+    lv_obj_t *size = lv_label_create(lv_screen_active());
+    lv_label_set_text(size, LV_SYMBOL_EJECT);
+    lv_obj_add_flag(size, LV_OBJ_FLAG_CLICKABLE);
+    lv_obj_align(size, LV_ALIGN_TOP_MID, 50, 0);
+    lv_obj_add_event_cb(size, sketchpad_toolbar_event_cb, LV_EVENT_ALL, &sketchpad_toolbar_width);
+}
+
+static void sketchpad_toolbar_event_cb(lv_event_t *e)
+{
+    lv_coord_t *toolbar_opt = (lv_coord_t *)lv_event_get_user_data(e);
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t *obj = (lv_obj_t *)lv_event_get_target(e);
+    lv_100ask_sketchpad_t *_sketchpad_t = (lv_100ask_sketchpad_t *)_sketchpad;
+
+    if (code == LV_EVENT_CLICKED)
+    {
+        if ((*toolbar_opt) == LV_100ASK_SKETCHPAD_TOOLBAR_OPT_DELETE)
+        {
+            Serial.println("Delete button clicked");
+            lv_canvas_fill_bg(_sketchpad, lv_color_hex3(0xccc), LV_OPA_COVER);
+        }
+        else if ((*toolbar_opt) == LV_100ASK_SKETCHPAD_TOOLBAR_OPT_WIDTH)
+        {
+            Serial.println("Width button clicked");
+            static lv_coord_t sketchpad_toolbar_width = LV_100ASK_SKETCHPAD_TOOLBAR_OPT_WIDTH;
+            lv_obj_t *slider = lv_slider_create(lv_screen_active());
+            lv_obj_set_width(slider, lv_pct(90));
+            lv_slider_set_value(slider, (int32_t)(_sketchpad_t->line_rect_dsc.width), LV_ANIM_OFF);
+            // lv_obj_align_to(slider, obj, LV_ALIGN_OUT_TOP_MID, 0, 0);
+            lv_obj_align(slider, LV_ALIGN_CENTER, 0, 0);
+            lv_obj_add_event_cb(slider, toolbar_set_event_cb, LV_EVENT_ALL, &sketchpad_toolbar_width);
+            lv_obj_move_foreground(slider);
+        }
+    }
+}
+static void toolbar_set_event_cb(lv_event_t *e)
+{
+    lv_coord_t *toolbar_opt = (lv_coord_t *)lv_event_get_user_data(e);
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t *obj = (lv_obj_t *)lv_event_get_target(e);
+    lv_100ask_sketchpad_t *sketchpad = (lv_100ask_sketchpad_t *)_sketchpad;
+
+    if (code == LV_EVENT_RELEASED)
+    {
+        if (*(toolbar_opt) == LV_100ASK_SKETCHPAD_TOOLBAR_OPT_WIDTH)
+        {
+            lv_obj_del(obj);
+        }
+    }
+    else if (code == LV_EVENT_VALUE_CHANGED)
+    {
+        if ((*toolbar_opt) == LV_100ASK_SKETCHPAD_TOOLBAR_OPT_WIDTH)
+        {
+            sketchpad->line_rect_dsc.width = lv_slider_get_value(obj);
+        }
+    }
 }
 
 // Loop that runs on main loop of the Ardunio
 void app_controller_run(void)
 {
-    // if (_drawline_flag)
-    // {
-    //     lv_layer_t layer;
-    //     lv_canvas_init_layer(_sketchpad, &layer);
-    //     void* ptr3 =((lv_100ask_sketchpad_t *)_sketchpad)->draw_buf->data;
-
-    //       Serial.printf("_drawline_flag: draw buffer address = %08x\r\n", ptr3);
-
-    //     lv_draw_line_dsc_t dsc;
-    //     lv_draw_line_dsc_init(&dsc);
-    //     dsc.color = lv_palette_main(LV_PALETTE_RED);
-    //     dsc.width = 4;
-    //     dsc.round_end = 1;
-    //     dsc.round_start = 1;
-    //     dsc.p1.x = 15;
-    //     dsc.p1.y = 15;
-    //     dsc.p2.x = 35;
-    //     dsc.p2.y = 10;
-    //     lv_draw_line(&layer, &dsc);
-
-    //     lv_canvas_finish_layer(_sketchpad, &layer);
-    //     Serial.println("Drawn a line");
-    //     _drawline_flag = 0;
-    // }
 }
--- lib/sketchpad/sketchpad.c
+++ lib/sketchpad/sketchpad.c
@@ -22,12 +22,8 @@
  **********************/
 static void lv_100ask_sketchpad_constructor(const lv_obj_class_t *class_p, lv_obj_t *obj);
 static void lv_100ask_sketchpad_destructor(const lv_obj_class_t *class_p, lv_obj_t *obj);
-static void lv_100ask_sketchpad_toolbar_constructor(const lv_obj_class_t *class_p, lv_obj_t *obj);
-static void lv_100ask_sketchpad_toolbar_destructor(const lv_obj_class_t *class_p, lv_obj_t *obj);
 static void lv_100ask_sketchpad_event(const lv_obj_class_t *class_p, lv_event_t *e);
-static void lv_100ask_sketchpad_toolbar_event(const lv_obj_class_t *class_p, lv_event_t *e);
-static void sketchpad_toolbar_event_cb(lv_event_t *e);
-static void toolbar_set_event_cb(lv_event_t *e);
+
 
 /**********************
  *  STATIC VARIABLES
@@ -41,15 +37,7 @@ const lv_obj_class_t lv_100ask_sketchpad_class = {
     .name = "sketchpad",
 };
 
-const lv_obj_class_t lv_100ask_sketchpad_toolbar_class = {
-    .constructor_cb = lv_100ask_sketchpad_toolbar_constructor,
-    .destructor_cb = lv_100ask_sketchpad_toolbar_destructor,
-    .event_cb = lv_100ask_sketchpad_toolbar_event,
-    .width_def = LV_SIZE_CONTENT,
-    .height_def = LV_SIZE_CONTENT,
-    .base_class = &lv_obj_class,
-    .name = "sketchpad_toolbar",
-};
+
 
 /**********************
  *      MACROS
@@ -87,13 +75,10 @@ static void lv_100ask_sketchpad_constructor(const lv_obj_class_t *class_p, lv_ob
     sketchpad->line_rect_dsc.round_start = true;
     sketchpad->line_rect_dsc.round_end = true;
     sketchpad->line_rect_dsc.color = lv_palette_main(LV_PALETTE_RED);
-    // sketchpad->line_rect_dsc.opa = LV_OPA_COVER;
 
     lv_obj_add_flag(obj, LV_OBJ_FLAG_CLICKABLE);
 
-    /*toolbar*/
-    // lv_obj_t * toolbar = lv_obj_class_create_obj(&lv_100ask_sketchpad_toolbar_class, obj);
-    // lv_obj_class_init_obj(toolbar);
+
 
     LV_TRACE_OBJ_CREATE("finished");
 }
@@ -127,7 +112,6 @@ static void lv_100ask_sketchpad_event(const lv_obj_class_t *class_p, lv_event_t 
 
     static lv_coord_t last_x = -32768, last_y = -32768;
 
-
     if (code == LV_EVENT_PRESSING)
     {
         lv_indev_t *indev = lv_indev_active();
@@ -138,9 +122,11 @@ static void lv_100ask_sketchpad_event(const lv_obj_class_t *class_p, lv_event_t 
 
         lv_indev_get_point(indev, &point);
 
+        // LV_LOG_USER("point: %d, %d", point.x, point.y);
+        // LV_LOG_USER("sketchpad point: %d, %d", sketchpad->pos.x, sketchpad->pos.y);
+
         point.x = point.x - sketchpad->pos.x;
         point.y = point.y - sketchpad->pos.y;
-        LV_LOG_USER("point: %d, %d", point.x, point.y);
         /*Release or first use*/
         if ((last_x != -32768) && (last_y != -32768))
         {
@@ -153,7 +139,6 @@ static void lv_100ask_sketchpad_event(const lv_obj_class_t *class_p, lv_event_t 
             sketchpad->line_rect_dsc.p2.y = point.y;
 
             lv_draw_line(&layer, &sketchpad->line_rect_dsc);
-            LV_LOG_USER("Should draw a line");
             lv_canvas_finish_layer(obj, &layer);
 
             lv_obj_send_event(obj, LV_EVENT_VALUE_CHANGED, NULL);
@@ -161,23 +146,6 @@ static void lv_100ask_sketchpad_event(const lv_obj_class_t *class_p, lv_event_t 
 
         last_x = point.x;
         last_y = point.y;
-
-        // lv_layer_t layer;
-        // lv_canvas_init_layer(obj, &layer);
-
-        // lv_draw_line_dsc_t dsc;
-        // lv_draw_line_dsc_init(&dsc);
-        // dsc.color = lv_palette_main(LV_PALETTE_RED);
-        // dsc.width = 4;
-        // dsc.round_end = 1;
-        // dsc.round_start = 1;
-        // dsc.p1.x = point.x;
-        // dsc.p1.y = point.y;
-        // dsc.p2.x = point.x + 10;
-        // dsc.p2.y = point.y + 10;
-        // lv_draw_line(&layer, &dsc);
-
-        // lv_canvas_finish_layer(obj, &layer);
     }
 
     /*Loosen the brush*/
@@ -191,122 +159,6 @@ static void lv_100ask_sketchpad_event(const lv_obj_class_t *class_p, lv_event_t 
         lv_obj_refr_pos(obj);
         sketchpad->pos.x = lv_obj_get_x(obj);
         sketchpad->pos.y = lv_obj_get_y(obj);
-    }
-}
-
-/*toolbar*/
-static void lv_100ask_sketchpad_toolbar_constructor(const lv_obj_class_t *class_p, lv_obj_t *obj)
-{
-    LV_UNUSED(class_p);
-    LV_TRACE_OBJ_CREATE("begin");
-
-    lv_obj_add_flag(obj, LV_OBJ_FLAG_CLICKABLE);
-    lv_obj_align(obj, LV_ALIGN_TOP_MID, 0, 0);
-    lv_obj_set_flex_grow(obj, 1);
-    lv_obj_set_flex_flow(obj, LV_FLEX_FLOW_ROW);
-
-    static lv_coord_t sketchpad_toolbar_cw = LV_100ASK_SKETCHPAD_TOOLBAR_OPT_CW;
-    lv_obj_t *color = lv_label_create(obj);
-    lv_label_set_text(color, LV_SYMBOL_EDIT);
-    lv_obj_add_flag(color, LV_OBJ_FLAG_CLICKABLE);
-    lv_obj_add_event_cb(color, sketchpad_toolbar_event_cb, LV_EVENT_ALL, &sketchpad_toolbar_cw);
-
-    static lv_coord_t sketchpad_toolbar_width = LV_100ASK_SKETCHPAD_TOOLBAR_OPT_WIDTH;
-    lv_obj_t *size = lv_label_create(obj);
-    lv_label_set_text(size, LV_SYMBOL_EJECT);
-    lv_obj_add_flag(size, LV_OBJ_FLAG_CLICKABLE);
-    lv_obj_add_event_cb(size, sketchpad_toolbar_event_cb, LV_EVENT_ALL, &sketchpad_toolbar_width);
-
-    LV_TRACE_OBJ_CREATE("finished");
-}
-
-static void lv_100ask_sketchpad_toolbar_destructor(const lv_obj_class_t *class_p, lv_obj_t *obj)
-{
-}
-
-static void lv_100ask_sketchpad_toolbar_event(const lv_obj_class_t *class_p, lv_event_t *e)
-{
-    LV_UNUSED(class_p);
-
-    lv_res_t res;
-
-    /*Call the ancestor's event handler*/
-    res = lv_obj_event_base(MY_CLASS, e);
-    if (res != LV_RES_OK)
-        return;
-
-    lv_event_code_t code = lv_event_get_code(e);
-    lv_obj_t *obj = lv_event_get_target(e);
-
-    if (code == LV_EVENT_PRESSING)
-    {
-        lv_indev_t *indev = lv_indev_get_act();
-        if (indev == NULL)
-            return;
-
-        lv_point_t vect;
-        lv_indev_get_vect(indev, &vect);
-
-        lv_coord_t x = lv_obj_get_x(obj) + vect.x;
-        lv_coord_t y = lv_obj_get_y(obj) + vect.y;
-        lv_obj_set_pos(obj, x, y);
-    }
-}
-
-static void sketchpad_toolbar_event_cb(lv_event_t *e)
-{
-    lv_coord_t *toolbar_opt = lv_event_get_user_data(e);
-    lv_event_code_t code = lv_event_get_code(e);
-    lv_obj_t *obj = lv_event_get_target(e);
-    lv_obj_t *toolbar = lv_obj_get_parent(obj);
-    lv_obj_t *sketchpad = lv_obj_get_parent(toolbar);
-    lv_100ask_sketchpad_t *sketchpad_t = (lv_100ask_sketchpad_t *)sketchpad;
-
-    if (code == LV_EVENT_CLICKED)
-    {
-        if ((*toolbar_opt) == LV_100ASK_SKETCHPAD_TOOLBAR_OPT_CW)
-        {
-            static lv_coord_t sketchpad_toolbar_cw = LV_100ASK_SKETCHPAD_TOOLBAR_OPT_CW;
-            //            lv_obj_t * cw = lv_colorwheel_create(sketchpad, true);
-            //            lv_obj_align_to(cw, obj, LV_ALIGN_OUT_BOTTOM_MID, 0, 0);
-            //            lv_obj_add_event_cb(cw, toolbar_set_event_cb, LV_EVENT_RELEASED, &sketchpad_toolbar_cw);
-        }
-        else if ((*toolbar_opt) == LV_100ASK_SKETCHPAD_TOOLBAR_OPT_WIDTH)
-        {
-            static lv_coord_t sketchpad_toolbar_width = LV_100ASK_SKETCHPAD_TOOLBAR_OPT_WIDTH;
-            lv_obj_t *slider = lv_slider_create(sketchpad);
-            lv_slider_set_value(slider, (int32_t)(sketchpad_t->line_rect_dsc.width), LV_ANIM_OFF);
-            lv_obj_align_to(slider, obj, LV_ALIGN_OUT_BOTTOM_MID, 0, 0);
-            lv_obj_add_event_cb(slider, toolbar_set_event_cb, LV_EVENT_ALL, &sketchpad_toolbar_width);
-        }
-    }
-}
-
-static void toolbar_set_event_cb(lv_event_t *e)
-{
-    lv_coord_t *toolbar_opt = lv_event_get_user_data(e);
-    lv_event_code_t code = lv_event_get_code(e);
-    lv_obj_t *obj = lv_event_get_target(e);
-    lv_100ask_sketchpad_t *sketchpad = (lv_100ask_sketchpad_t *)lv_obj_get_parent(obj);
-
-    if (code == LV_EVENT_RELEASED)
-    {
-        if ((*toolbar_opt) == LV_100ASK_SKETCHPAD_TOOLBAR_OPT_CW)
-        {
-            //            sketchpad->line_rect_dsc.color = lv_colorwheel_get_rgb(obj);
-            //            lv_obj_del(obj);
-            LV_LOG_USER("Nothing done.TODO");
-        }
-        else if (*(toolbar_opt) == LV_100ASK_SKETCHPAD_TOOLBAR_OPT_WIDTH)
-        {
-            lv_obj_del(obj);
-        }
-    }
-    else if (code == LV_EVENT_VALUE_CHANGED)
-    {
-        if ((*toolbar_opt) == LV_100ASK_SKETCHPAD_TOOLBAR_OPT_WIDTH)
-        {
-            sketchpad->line_rect_dsc.width = (lv_coord_t)lv_slider_get_value(obj);
-        }
+        // LV_LOG_USER("pos.x,y changed: %d, %d", sketchpad->pos.x, sketchpad->pos.y);
     }
 }
